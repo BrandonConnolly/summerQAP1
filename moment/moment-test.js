0665926dcb6e39e8106f824cb7412e6c5bfe38ca
BrandonConnolly <119525545+BrandonConnolly@users.noreply.github.com>
2023-05-17 21:21:54 -0230

    Update moment-test.js

--- moment/moment-test.js
+++ moment/moment-test.js
@@ -1,1 +1,8 @@
 
+const moment = require("moment");
+
+const now = moment();
+console.log(now.format("YYYY-MM-DD"));
+
+// This is a simple node that can display dates
+// Running this code will give you todays date
