2b0d60cfecaa93d5620b090c3661871e758c7437
BrandonConnolly <119525545+BrandonConnolly@users.noreply.github.com>
2023-05-17 21:20:45 -0230

    Add files via upload

---
+++ moment-test.js
@@ -0,0 +1,7 @@
+const moment = require("moment");
+
+const now = moment();
+console.log(now.format("YYYY-MM-DD"));
+
+// This is a simple node that can display dates
+// Running this code will give you todays date
